ac2e8aa29662b388423ba7a19a76dcf1096f619a
Jonathan-Xue <jonathanxue50@gmail.com>
2020-04-10 04:14:25 -0500

    Server Debug

--- server/__init__.py
+++ server/__init__.py
@@ -1,10 +1,14 @@
 import os
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from flask_pymongo import pymongo
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+cors = CORS(app, resources={r"/*": {"origins": "*"}}) 
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['CLOUD_SQL_URI']
 sql_db = SQLAlchemy(app)
 
--- server/sql/sql_template.py
+++ server/sql/sql_template.py
@@ -1,52 +1,113 @@
 from flask import request
 from server import app, sql_db
 
-@app.route('/create-entry', methods=['POST'])
-def create_entry():
-    pass
+@app.route('/entries', methods=['GET','POST'])
+def entries():
+    if request.method == 'GET':
+        # Query
+        result = sql_db.engine.execute('SELECT * FROM csGrade')
+        rows = result.fetchall()
 
-@app.route('/delete-entry', methods=['DELETE'])
-def delete_entry():
-    pass
+        # Parse Output
+        row_dicts = [dict(row) for row in rows]
+        row_data = { 'data': row_dicts, 'length': len(row_dicts) }
 
-@app.route('/find-entries', methods=['GET'])
-def find_entries():
-    pass
+        # Return
+        return row_data
+    elif request.method == 'POST':
+        # Parse Arguments
+        data = request.json
+        create_data = (data['courseNo'], data['courseName'], data['year'], data['term'],
+                       data['primaryInstructor'], data['aPlus'], data['a'], data['aMinus'],
+                       data['bPlus'], data['b'], data['bMinus'], data['cPlus'], data['c'],
+                       data['cMinus'], data['dPlus'], data['d'], data['dMinus'], data['f'])
 
-@app.route('/create-course', methods=['POST'])
-def create_course():
-    pass
+        # Query
+        create_query = """INSERT INTO csGrade
+                   (courseNo, courseName, year, term, primaryInstructor, aPlus, a, aMinus, bPlus, b, bMinus, cPlus, c, cMinus, dPlus, d, dMinus, f)
+                   VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+        sql_db.engine.execute(create_query, create_data)
 
-@app.route('/update-course', methods=['PUT'])
-def update_course():
-    pass
+        # Return
+        return {}
+    else:
+        print("Error")
+        return {}
 
-@app.route('/delete-course', methods=['DELETE'])
-def delete_course():
-    pass
+@app.route('/entries/<courseNo>/<courseName>/<year>/<term>/<primaryInstructor>', methods=['PUT', 'DELETE'])
+def entries_dup(courseNo, courseName, year, term, primaryInstructor):
+    if request.method == 'PUT':
+        # Return
+        return {}
+    elif request.method == 'DELETE':
+        # Parse Arguments
+        delete_data = (courseNo, courseName, year, term, primaryInstructor)
 
-@app.route('/find-course', methods=['GET'])
-def find_course():
-    pass
+        # Query
+        delete_query = """DELETE FROM csGrade WHERE courseNo = %s AND courseName = %s AND year = %s AND term = %s AND primaryInstructor = %s;""" 
+        sql_db.engine.execute(delete_query, delete_data)
 
-@app.route('/entries', methods=['GET'])
-def get_all_entries():
-    result = sql_db.engine.execute('SELECT * FROM csGrade')
-    rows = result.fetchall()
-    row_dicts = [dict(row) for row in rows]
-    row_data = { 'data': row_dicts, 'length': len(row_dicts) }
-    return row_data
+        # Return
+        return {}
+    else:
+        print("Error")
+        return {}
 
-@app.route('/courses', methods=['GET'])
-def get_all_courses():
-    result = sql_db.engine.execute('SELECT * FROM csCourse')
-    rows = result.fetchall()
-    row_dicts = [dict(row) for row in rows]
-    row_data = { 'data': row_dicts, 'length': len(row_dicts) }
-    return row_data
+@app.route('/courses', methods=['GET', 'POST'])
+def courses():
+    if request.method == 'GET':
+        # Query
+        result = sql_db.engine.execute('SELECT * FROM csCourse')
+        rows = result.fetchall()
 
-@app.route('/sql', methods=['GET'])
-def sql_template():
-    result = sql_db.engine.execute('SELECT id FROM test WHERE id = %s', ('Hi'))
-    rows = result.fetchall()
-    return rows[0]['id']
+        # Parse Output
+        row_dicts = [dict(row) for row in rows]
+        row_data = { 'data': row_dicts, 'length': len(row_dicts) }
+
+        # Return
+        return row_data
+    elif request.method == 'POST':
+        # Parse Arguments
+        data = request.json
+        create_data = (data['courseNo'], data['courseName'], data['courseDesc'])
+
+        # Query
+        create_query = """INSERT INTO csCourse
+                   (courseNo, courseName, courseDesc)
+                   VALUES (%s, %s, %s);"""
+        sql_db.engine.execute(create_query, create_data)
+
+        # Return
+        return {}
+    else:
+        print("Error")
+        return {}
+
+@app.route('/courses/<courseNo>/<courseName>', methods=['PUT', 'DELETE'])
+def courses_dup(courseNo, courseName):
+    if request.method == 'PUT':
+        # Parse Arguments
+        data = request.json
+        update_data = (data['courseDesc'], courseNo, courseName)
+
+        # Query
+        update_query = """UPDATE csCourse
+                       SET courseDesc = %s
+                       WHERE courseNo = %s AND courseName = %s;"""
+        sql_db.engine.execute(update_query, update_data)
+
+        # Return
+        return {}
+    elif request.method == 'DELETE':
+        # Parse Arguments
+        delete_data = (courseNo, courseName)
+
+        # Query
+        delete_query = """DELETE FROM csCourse WHERE courseNo = %s AND courseName = %s;"""
+        sql_db.engine.execute(delete_query, delete_data)
+
+        # Return
+        return {}
+    else:
+        print("Error")
+        return {}

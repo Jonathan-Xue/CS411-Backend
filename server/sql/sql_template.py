f78c6430cebc541d9b4d8d2ea34e563e38bbf44c
Isaac Park <keonp2@illinois.edu>
2020-04-09 20:25:49 -0400

    cleaned data again and imported into cloud sql

--- server/sql/sql_template.py
+++ server/sql/sql_template.py
@@ -6,8 +6,6 @@ from server import app, sql_db
 #     def __repr__(self):
 #         return "" # TODO
 
-# Note: Don't pip install specific versions
-# https://realpython.com/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/
 @app.route('/sql', methods=['GET'])
 def sql_template():
     result = sql_db.engine.execute('SELECT id FROM test WHERE id = %s', ('Hi'))

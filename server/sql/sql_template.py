5083812051ba832e7a647eb69ee519ce2ae1d279
Isaac Park <keonp2@illinois.edu>
2020-04-09 23:54:08 -0400

    updated schema + added sql endpoint skeleton code

--- server/sql/sql_template.py
+++ server/sql/sql_template.py
@@ -1,10 +1,47 @@
+from flask import request
 from server import app, sql_db
 
-# class Course(sql_db.Model):
-#     # TODO
-#
-#     def __repr__(self):
-#         return "" # TODO
+@app.route('/create-entry', methods=['POST'])
+def create_entry():
+    pass
+
+@app.route('/delete-entry', methods=['DELETE'])
+def delete_entry():
+    pass
+
+@app.route('/find-entries', methods=['GET'])
+def find_entries():
+    pass
+
+@app.route('/create-course', methods=['POST'])
+def create_course():
+    pass
+
+@app.route('/update-course', methods=['PUT'])
+def update_course():
+    pass
+
+@app.route('/delete-course', methods=['DELETE'])
+def delete_course():
+    pass
+
+@app.route('/find-course', methods=['GET'])
+def find_course():
+    pass
+
+@app.route('/entries', methods=['GET'])
+def get_all_entries():
+    result = sql_db.engine.execute('SELECT * FROM csGrade')
+    rows = result.fetchall()
+    row_dicts = [dict(row) for row in rows]
+    return row_dicts
+
+@app.route('/courses', methods=['GET'])
+def get_all_courses():
+    result = sql_db.engine.execute('SELECT * FROM csCourse')
+    rows = result.fetchall()
+    row_dicts = [dict(row) for row in rows]
+    return row_dicts
 
 @app.route('/sql', methods=['GET'])
 def sql_template():
